f2a283428dc11cd58d55329975d5e639fb98e4bb
IT-Imani <IT-Imani@IT-IMAN>
2022-08-09 15:07:44 +0430

    fn variable

--- partitioner.js
+++ partitioner.js
@@ -1,84 +1,102 @@
-$(document).ready(function () {
-    var religionList = ["شیعه", "اهل سنت", "مسیحیت", "کلیمی و ارمنی", "زرتشتی", "اهل حق", "دراویش صوفی", "بهائیت", "سایر مذاهب"];
-    var religionPercent = [];
-    for (var i = 0; i < religionList.length; i++) {
-        religionPercent.push(0);
-    }
-    var StepNumber = 5;
-    var StartNumber = 5;
-
-    var itemHtml = $(".partitionerCntr").html();
-    $(".partitionerCntr").empty();
-
-    var partitionerAddHtml = document.getElementById("partitionerAddItem").outerHTML;
-    $("#partitionerAddItem").remove();
-
-    function SetPartitioner() {
-        var sumPercent = 0;
-        //--------------------------------------------------- Set DropDown religion
-        var optionR = '';
-        for (var i = 0; i < religionList.length; i++) {
-            if (religionPercent[i] == 0) {
-                optionR += '<option value="' + i + '">' + religionList[i] + '</option>';
-            } else {
-                sumPercent += parseInt(religionPercent[i]);
-            }
-        }
-        //--------------------------------------------------/ Set DropDown religion
-        //-------------------------------------------------- Create Partitioner Items
-        var partitionerCntrHtml = "";
-        for (var i = 0; i < religionPercent.length; i++) {
-            if (religionPercent[i] > 0) {
-                var newItemHtml = itemHtml
-                    .replace("#ItemName", religionList[i])
-                    .replace("#ItemPercentText", religionPercent[i])
-                    .replace("#ItemIndex", i)
-                    .replace("#ItemPercentNumber", religionPercent[i] - 1)
-                    ;
-                partitionerCntrHtml = partitionerCntrHtml + newItemHtml;
-            }
-        }
-
-        $(".partitionerCntr").html(partitionerCntrHtml + partitionerAddHtml);
-
-
-        //-------------------------------------------------/ Create Partitioner Items
-        //--------------------------------------------------- Set DropDown religion
-        var optionP = '';
-        var min = StartNumber;
-        var max = 100 - sumPercent;
-        if (religionPercent.length - $(".partitionerCntr").children(".partitionItem").length == 1) {
-            min = max;
-        }
-        for (var i = min; i <= max; i = i + StepNumber) {
-            optionP += '<option value="' + i + '">' + i + '</option>';
-        }
-        //--------------------------------------------------/ Set DropDown religion
-
-        $('.DropPartitionItem').empty();
-        $('.DropPartitionItem').append(optionR);
-
-        $('.DropReligionPercent').empty();
-        $('.DropReligionPercent').append(optionP);
-
-        $('#partitionerAddItem').css('width', (100 - sumPercent - 3) + '%');
-        if (sumPercent < 100) {
-            $('#partitionerAddItem').show();
-        } else {
-            $('#partitionerAddItem').hide();
-        }
-
-    }
+var arrayList = [];
+var arrayPercent = [];
+var stepNumber = 5;
+var startNumber = 5;
+var itemTemplate = "";
+var btnTemplate = "";
+Usage: jQuery.fn.SetPartitioner = function (param) {
+    var currentObject = $(this);
     $("body").on('click', '.RemovePercent', function () {
-        var ItemIndex = $(this).attr("id");
-        religionPercent[parseInt(ItemIndex)] = 0;
-        SetPartitioner();
+        debugger
+        var itemIndex = $(this).attr("id");
+        arrayPercent[parseInt(itemIndex)] = 0;
+        SetPartition(currentObject);
     });
     $("body").on('click', '.partitionerAddBtn', function () {
-        var PartitionItem = $('.DropPartitionItem').val();
-        var ReligionPercent = $('.DropReligionPercent').val();
-        religionPercent[PartitionItem] = ReligionPercent;
-        SetPartitioner();
+        debugger
+        var partitionItem = $('.DropPartitionItem').val();
+        var itemPercent = $('.DropArrayPercent').val();
+        arrayPercent[partitionItem] = itemPercent;
+        SetPartition(currentObject);
     });
-    SetPartitioner();
-});
+
+    //--------------------------------------------------- Initialize
+    debugger
+    arrayList = param.arrayList;
+    if (param.stepNumber) {
+        stepNumber = param.stepNumber;
+    }
+    if (param.stepNumber) {
+        startNumber = param.startNumber;
+    }
+    //--------------------------------------------------- Variable Defining
+
+    for (var i = 0; i < arrayList.length; i++) {
+        arrayPercent.push(0);
+    }
+    debugger
+
+    itemHtml = currentObject.children(".partitionerBox").html();
+    currentObject.children(".partitionerBox").empty();
+
+    btnTemplate = currentObject.children(".partitionerAddItemCntr").html();
+    currentObject.children(".partitionerAddItemCntr").remove();
+
+    SetPartition(currentObject);
+}
+
+function SetPartition(currentObject) {
+    debugger
+    var sumPercent = 0;
+    //--------------------------------------------------- Set DropDown religion option
+    var optionR = '';
+    for (var i = 0; i < arrayList.length; i++) {
+        if (arrayPercent[i] == 0) {
+            optionR += '<option value="' + i + '">' + arrayList[i] + '</option>';
+        } else {
+            sumPercent += parseInt(arrayPercent[i]);
+        }
+    }
+    //-------------------------------------------------- Create Partitioner Items
+    var partitionerBoxHtml = "";
+    for (var i = 0; i < arrayPercent.length; i++) {
+        if (arrayPercent[i] > 0) {
+            var newItemHtml = itemHtml
+                .replace("#ItemName", arrayList[i])
+                .replace("#ItemPercentText", arrayPercent[i])
+                .replace("#itemIndex", i)
+                .replace("#ItemPercentNumber", arrayPercent[i] - 1)
+                ;
+            partitionerBoxHtml = partitionerBoxHtml + newItemHtml;
+        }
+    }
+    currentObject.children(".partitionerBox").html(partitionerBoxHtml + btnTemplate);
+    //--------------------------------------------------- Set DropDown religion option
+    var optionP = '';
+    var min = startNumber;
+    var max = 100 - sumPercent;
+    if (arrayPercent.length - currentObject.children(".partitionerBox").children(".partitionItem").length == 1) {
+        min = max;
+    }
+    for (var i = min; i <= max; i = i + stepNumber) {
+        optionP += '<option value="' + i + '">' + i + '</option>';
+    }
+    //-------------------------------------------------- Append DropDown Items
+    currentObject.find('.DroppartitionItem').empty();
+    currentObject.find('.DroppartitionItem').append(optionR);
+
+    currentObject.find('.DroparrayPercent').empty();
+    currentObject.find('.DroparrayPercent').append(optionP);
+
+    currentObject.find('#partitionerAddItem').css('width', (100 - sumPercent - 3) + '%');
+    if (sumPercent < 100) {
+        currentObject.find('#partitionerAddItem').show();
+    } else {
+        currentObject.find('#partitionerAddItem').hide();
+    }
+}
+//-------------------------------------------------- Event Listener
+
+//SetPartition();
+
+
