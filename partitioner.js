8fe68617fea242c1d34668de56e04bfa17e05cfb
IT-Imani <IT-Imani@IT-IMAN>
2022-08-10 13:59:14 +0430

    active multi control

--- partitioner.js
+++ partitioner.js
@@ -1,102 +1,95 @@
-var arrayList = [];
-var arrayPercent = [];
-var stepNumber = 5;
-var startNumber = 5;
-var itemTemplate = "";
-var btnTemplate = "";
 Usage: jQuery.fn.SetPartitioner = function (param) {
-    var currentObject = $(this);
-    $("body").on('click', '.RemovePercent', function () {
-        debugger
-        var itemIndex = $(this).attr("id");
-        arrayPercent[parseInt(itemIndex)] = 0;
-        SetPartition(currentObject);
-    });
-    $("body").on('click', '.partitionerAddBtn', function () {
-        debugger
-        var partitionItem = $('.DropPartitionItem').val();
-        var itemPercent = $('.DropArrayPercent').val();
-        arrayPercent[partitionItem] = itemPercent;
-        SetPartition(currentObject);
-    });
-
-    //--------------------------------------------------- Initialize
     debugger
-    arrayList = param.arrayList;
+    var currentObject = $(this);
+    //-------------------------------------------------- Event Listener
+    $("body").on('click', '.RemoveItem', function () {
+        // debugger
+        var rootItem = $(this).closest(".partitionerCntr");
+        itemPercent = rootItem.attr("itemPercent").split(',');
+        var itemIndex = $(this).attr("index");
+        itemPercent[parseInt(itemIndex)] = 0;
+        rootItem.attr("itemPercent", itemPercent.join(", "));
+        SetPartition(currentObject);
+    });
+    $("body").on('click', '.addBtn', function () {
+        // debugger
+        var rootItem = $(this).closest(".partitionerCntr");
+        itemPercent = rootItem.attr("itemPercent").split(',');
+        var pItem = rootItem.find('.DItem').val();
+        var itemPercentValue = rootItem.find('.DPercent').val();
+        itemPercent[pItem] = itemPercentValue;
+        rootItem.attr("itemPercent", itemPercent.join(", "));
+        SetPartition(currentObject);
+    });
+    //--------------------------------------------------- Initialize
+    // debugger
+    currentObject.attr("itemList", param.itemList.join(","));
+    var itemPercent = [];
+    for (var i = 0; i < param.itemList.length; i++) { itemPercent.push("0"); }
+    currentObject.attr("itemPercent", itemPercent.join(","));
     if (param.stepNumber) {
-        stepNumber = param.stepNumber;
+        currentObject.attr("stepNumber", param.stepNumber);
+    } else {
+        currentObject.attr("stepNumber", 5);
     }
-    if (param.stepNumber) {
-        startNumber = param.startNumber;
+    if (param.startNumber) {
+        currentObject.attr("startNumber", param.startNumber);
+    } else {
+        currentObject.attr("startNumber", 5);
     }
     //--------------------------------------------------- Variable Defining
-
-    for (var i = 0; i < arrayList.length; i++) {
-        arrayPercent.push(0);
-    }
-    debugger
-
-    itemHtml = currentObject.children(".partitionerBox").html();
-    currentObject.children(".partitionerBox").empty();
-
-    btnTemplate = currentObject.children(".partitionerAddItemCntr").html();
-    currentObject.children(".partitionerAddItemCntr").remove();
-
     SetPartition(currentObject);
 }
 
 function SetPartition(currentObject) {
     debugger
+    var itemList = currentObject.attr("itemList").split(',');
+    var itemPercent = currentObject.attr("itemPercent").split(',');
+    var stepNumber = parseInt(currentObject.attr("stepNumber"));
+    var startNumber = parseInt(currentObject.attr("startNumber"));
+
     var sumPercent = 0;
-    //--------------------------------------------------- Set DropDown religion option
+    //--------------------------------------------------- Set DropDown itemList option
     var optionR = '';
-    for (var i = 0; i < arrayList.length; i++) {
-        if (arrayPercent[i] == 0) {
-            optionR += '<option value="' + i + '">' + arrayList[i] + '</option>';
+    for (var i = 0; i < itemList.length; i++) {
+        if (itemPercent[i] == 0) {
+            optionR += '<option value="' + i + '">' + itemList[i] + '</option>';
         } else {
-            sumPercent += parseInt(arrayPercent[i]);
+            sumPercent += parseInt(itemPercent[i]);
         }
     }
     //-------------------------------------------------- Create Partitioner Items
-    var partitionerBoxHtml = "";
-    for (var i = 0; i < arrayPercent.length; i++) {
-        if (arrayPercent[i] > 0) {
-            var newItemHtml = itemHtml
-                .replace("#ItemName", arrayList[i])
-                .replace("#ItemPercentText", arrayPercent[i])
-                .replace("#itemIndex", i)
-                .replace("#ItemPercentNumber", arrayPercent[i] - 1)
-                ;
-            partitionerBoxHtml = partitionerBoxHtml + newItemHtml;
+    // debugger
+    itemTemplate = currentObject.children(".pItemThem").html();
+    addItemTemplate = currentObject.children(".pAddThem").html();
+    currentObject.children(".pBox").empty();
+    for (var i = 0; i < itemPercent.length; i++) {
+        if (itemPercent[i] > 0) {
+            currentObject.children(".pBox").append(itemTemplate);
+            var newItem = currentObject.children(".pBox").find(".newitem");
+            newItem.css('width', (itemPercent[i] - 1) + '%');
+            newItem.find(".ItemName").html(itemList[i]);
+            newItem.find(".PercentNumber").html(itemPercent[i] + " %");
+            newItem.find(".RemoveItem").attr("index", i);
+            newItem.removeClass("newitem")
         }
     }
-    currentObject.children(".partitionerBox").html(partitionerBoxHtml + btnTemplate);
-    //--------------------------------------------------- Set DropDown religion option
-    var optionP = '';
-    var min = startNumber;
-    var max = 100 - sumPercent;
-    if (arrayPercent.length - currentObject.children(".partitionerBox").children(".partitionItem").length == 1) {
-        min = max;
-    }
-    for (var i = min; i <= max; i = i + stepNumber) {
-        optionP += '<option value="' + i + '">' + i + '</option>';
-    }
-    //-------------------------------------------------- Append DropDown Items
-    currentObject.find('.DroppartitionItem').empty();
-    currentObject.find('.DroppartitionItem').append(optionR);
 
-    currentObject.find('.DroparrayPercent').empty();
-    currentObject.find('.DroparrayPercent').append(optionP);
-
-    currentObject.find('#partitionerAddItem').css('width', (100 - sumPercent - 3) + '%');
+    //-------------------------------------------------- Set DropDown itemPercent option
     if (sumPercent < 100) {
-        currentObject.find('#partitionerAddItem').show();
-    } else {
-        currentObject.find('#partitionerAddItem').hide();
+        currentObject.children(".pBox").append(addItemTemplate);
+        var optionP = '';
+        var min = startNumber;
+        var max = 100 - sumPercent;
+        if (itemPercent.length - currentObject.children(".pBox").children(".pItem").length == 1) {
+            min = max;
+        }
+        for (var i = min; i <= max; i = i + stepNumber) {
+            optionP += '<option value="' + i + '">' + i + '</option>';
+        }
+        var addItem = currentObject.children(".pBox").children(".addBox");
+        addItem.css('width', (100 - sumPercent - 3) + '%');
+        addItem.find('.DItem').empty().append(optionR);
+        addItem.find('.DPercent').empty().append(optionP);
     }
 }
-//-------------------------------------------------- Event Listener
-
-//SetPartition();
-
-
